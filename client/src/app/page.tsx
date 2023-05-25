73e8216c1b2416f75c6491473f74fb98983d2439
Jeevan Rupacha <rupachasgvn@gmail.com>
2023-05-25 22:25:03 +0545

    package maange

--- client/src/app/page.tsx
+++ client/src/app/page.tsx
@@ -2,9 +2,9 @@ import Image from 'next/image'
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+    <main className='min-h-screen bg-primary-black'>
       <div>
-        <p>This is test</p>
+        <p> Test </p>
       </div>
     </main>
   )
--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -13,6 +13,18 @@ module.exports = {
         'gradient-conic':
           'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
       },
+      colors:{
+        'primary-black':'#2C2C2C',
+        'card-color':'#343434',
+        'wallet-msg':'#1C1F26',
+        'text-color':'#FFFFFF',
+        'blue':'#0044CC',
+        'button':'#00E472',
+        'line':'#D9D9D9',
+        'homecard':'#3A3A3A',
+        'form':'#32F694',
+        'formtext':'#CCCCCC',
+      }
     },
   },
   plugins: [],
